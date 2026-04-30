48cb67f06e590f77e95e3f84752ba0a830d7ea93
MarioAlonsoSolport <mario.alonso@solport.net>
2026-04-30 15:08:35 +0200

    feat:initial setup shared-ui

---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+import * as react_jsx_runtime from 'react/jsx-runtime';
+import { ReactNode } from 'react';
+
+declare const Button: ({ children }: {
+    children: ReactNode;
+}) => react_jsx_runtime.JSX.Element;
+
+export { Button };
---
+++ dist/index.js
@@ -0,0 +1,8 @@
+// src/components/Button.tsx
+import { jsx } from "react/jsx-runtime";
+var Button = ({ children }) => {
+  return /* @__PURE__ */ jsx("button", { style: { backgroundColor: "orange", color: "black" }, children });
+};
+export {
+  Button
+};
